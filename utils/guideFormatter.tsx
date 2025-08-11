c295fce46d58a6b605d3ec59263035755eb40acd
Paolo Amosso <39710874+Eisman111@users.noreply.github.com>
2025-08-11 16:54:09 +0200

    chore: Formatted last component

--- utils/guideFormatter.tsx
+++ utils/guideFormatter.tsx
@@ -1,5 +1,5 @@
-import React, {JSX} from "react";
-import {StyleSheet, Text, View} from "react-native";
+import React, { JSX } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export interface FormatterColors {
   text: string;
